000ec154e9b7b4b2e98c58e37ef74aa72a65fb3c
Alena Golovina <alenasgolovina@gmail.com>
2017-06-02 16:13:31 -0700

    add changes to styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,37 @@
 
 $(document).ready(function() {
-
   $("#user-info").submit(function() {
-
       var nameInput = $("input#name").val();
-      var trackInput = $("input#track").val();
+      //var trackInput = $("input#track").val();
+        $(".name").text(nameInput);
 
-      var inputCompUser = $("input:radio[name=compUser]:checked").val();
-      var inputDesign = $("input:radio[name=design]:checked").val();
-      var inputCompanySize = $("input:radio[name=companySize]:checked").val();
-      var inputApplication = $("input:radio[name=application]:checked").val();
+        var inputCompUser = $("input:radio[name=compUser]:checked").val();
 
-      $(".name").text(nameInput);
-      //$(".track")text(trackInput);
+        var inputDesign = $("input:radio[name=design]:checked").val();
 
-      if (inputCompUser === "Yes") {
-        $("#suggestTrackCss").hide();
-        $("#suggestTrackRuby").hide();
-        $("#suggestTrackCsharp").hide();
-        $("#suggestTrackJava").hide();
-        $("#learnTyping").hide();
-      } else {
-          if (inputDesign === "Yes") {
+        var inputCompanySize = $("input:radio[name=companySize]:checked").val();
 
-          }
+        var inputApplication = $("input:radio[name=application]:checked").val();
 
-           event.preventDefault();
 
-  });
+        //$(".track")text(trackInput);
 
-});
+        if (inputCompUser === "No") {
+          $("#learnTyping").show();
+
+        }
+      
+
+event.preventDefault();
+
+  ;)}
+;)}
+
+
+
+
+//$("#suggestTrackCss").hide();
+//$("#suggestTrackRuby").hide();
+//$("#suggestTrackCsharp").hide();
+//$("#suggestTrackJava").hide();
+//$("#learnTyping").hide();
