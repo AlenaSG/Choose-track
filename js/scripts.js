62fc2da9df8aeb74c9999b7eb356cfdd226c5263
Alena Golovina <alenasgolovina@gmail.com>
2017-06-02 13:28:14 -0700

    add variables and branching in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,48 @@
+
 $(document).ready(function() {
-  
+
+  $("#user-info").submit(function() {
+
+      var nameInput = $("input#name").val();
+      var trackInput = $("input#track").val();
+var inputCompUser = $("input:radio[name=compUser]:checked").val();
+//var inputDesign = $("input#design").val();
+var inputCompUser = $("input:radio[name=compUser]:checked").val();
+var inputCompUser = $("input:radio[name=compUser]:checked").val();
+var inputCompUser = $("input:radio[name=compUser]:checked").val();
+
+      $(".name").text(nameInput);
+      //$(".track")text(trackInput);
+      if (inputCompUser === "canUse") {
+
+            $("#suggestTrack").show();
+            $("#learnTyping").hide();
+          } else {
+
+            //if (inputDesign === true) {
+                  //$("#suggestTrack").show();
+                  //$("#learnTyping").hide();
+            //  } else {
+                //alert ("second else");
+                  $("#suggestTrack").hide();
+                  $("#learnTyping").show();
+                //}
+            //$("#suggestTrack").hide();
+            //$("#learnTyping").show();
+          }
+
+      //if (inputDesign === true) {
+            //$("#suggestTrack").show();
+            //$("#learnTyping").hide();
+        //  } else {
+            //$("#suggestTrack").hide();
+            //$("#learnTyping").show();
+          //}
+
+       //$("#suggestTrack").show();
+
+       event.preventDefault();
+
+  });
+
 });
