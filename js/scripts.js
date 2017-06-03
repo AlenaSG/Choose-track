70529f048063768d2d31c59b60c965c939d437db
Alena Golovina <alenasgolovina@gmail.com>
2017-06-02 17:01:33 -0700

    fix indentation in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,22 @@
 
 $(document).ready(function() {
   $("#user-info").submit(function() {
-      var nameInput = $("input#name").val();
+    var nameInput = $("input#name").val();
       //var trackInput = $("input#track").val();
-        $(".name").text(nameInput);
+    $(".name").text(nameInput);
 
-        var inputCompUser = $("input:radio[name=compUser]:checked").val();
-
-        var inputDesign = $("input:radio[name=design]:checked").val();
-
-        var inputCompanySize = $("input:radio[name=companySize]:checked").val();
-
-        var inputApplication = $("input:radio[name=application]:checked").val();
-
-
+    var inputCompUser = $("input:radio[name=compUser]:checked").val();
+    var inputDesign = $("input:radio[name=design]:checked").val();
+    var inputCompanySize = $("input:radio[name=companySize]:checked").val();
+    var inputApplication = $("input:radio[name=application]:checked").val();
         //$(".track")text(trackInput);
+    if (inputCompUser === "No") {
+      $("#learnTyping").show();
+    }
 
-        if (inputCompUser === "No") {
-          $("#learnTyping").show();
-
-        }
-      
-
-event.preventDefault();
-
+    event.preventDefault();
   ;)}
 ;)}
-
-
 
 
 //$("#suggestTrackCss").hide();
