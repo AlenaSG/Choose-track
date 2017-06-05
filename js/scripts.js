9f547ad2c5ed00271f17926e241f37bc95e927af
AlenaSG <alenasgolovina@gmail.com>
2017-06-04 23:48:37 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,32 @@
 
 $(document).ready(function() {
   $("#user-info").submit(function() {
+    $("#learnTyping").hide();
+    $("#suggestTrackCss").hide();
+    $("#suggestTrackJava").hide();
+    $("#suggestTrackCsharp").hide();
+    $("#suggestTrackRuby").hide();
+
     var nameInput = $("input#name").val();
-      //var trackInput = $("input#track").val();
     $(".name").text(nameInput);
 
     var inputCompUser = $("input:radio[name=compUser]:checked").val();
     var inputDesign = $("input:radio[name=design]:checked").val();
     var inputCompanySize = $("input:radio[name=companySize]:checked").val();
     var inputApplication = $("input:radio[name=application]:checked").val();
-        //$(".track")text(trackInput);
-    if (inputCompUser === "No") {
+
+    if (inputCompUser === "cantUse") {
       $("#learnTyping").show();
+    } else if (inputDesign === "frontEnd") {
+      $("#suggestTrackCss").show();
+    } else if (inputCompanySize === "small") {
+      $("#suggestTrackRuby").show();
+    } else if (inputApplication === "internalSoft") {
+      $("#suggestTrackCsharp").show();
+    } else if (inputApplication === "externalSoft") {
+      $("#suggestTrackJava").show();
     }
 
     event.preventDefault();
-  ;)}
-;)}
-
-
-//$("#suggestTrackCss").hide();
-//$("#suggestTrackRuby").hide();
-//$("#suggestTrackCsharp").hide();
-//$("#suggestTrackJava").hide();
-//$("#learnTyping").hide();
+  ;})
+;})
